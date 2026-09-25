f9fc2fe6ab2982bf1475dccf48f5e90399c663bc
Ipshita Patel <ipshita2908@gmail.com>
2026-09-25 11:38:49 +0530

    feat: add /api/predictor/history, /performance, and /evaluation/:season/:round routes

--- server/routes/predictorRoutes.js
+++ server/routes/predictorRoutes.js
@@ -1,4 +1,7 @@
 const router = require("express").Router()
-const {getUpcomingPrediction} = require("../controllers/predictorController")
+const {getUpcomingPrediction,getHistory,getEvaluation,getPerformance} = require("../controllers/predictorController")
 router.get("/upcoming",getUpcomingPrediction)
+router.get("/history",getHistory)
+router.get("/performance",getPerformance)
+router.get("/evaluation/:season/:round",getEvaluation)
 module.exports=router;
